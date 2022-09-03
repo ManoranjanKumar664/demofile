e378ef49920caae30fd0595e5241096e7ed25ad8
Manoranjan Kumar <baskeymanoranjan400@gmail.com>
2022-09-03 14:28:13 +0530

    many codes added

--- first.cpp
+++ first.cpp
@@ -1,5 +1,6 @@
 #include<iosteam.h>
 #include<conio.h>
+void codetowritename();
 void main()
 {
  clrscr();
@@ -9,5 +10,18 @@ cin>>a>>b;
 c=a+b;
 cout<<"sum="<<c;
  cout<<"hello world";
+ codetowritename();
  getch();
  }
+
+ void codetowritename()
+{
+ if(a<=10)
+{
+cout<<"manoranjan";
+}
+else
+{
+ cout<<"not valid";
+}
+}
