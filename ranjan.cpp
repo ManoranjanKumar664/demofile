36a6a68d736ac3c150c7e4dd6039207e0cd074cf
Manoranjan Kumar <baskeymanoranjan400@gmail.com>
2022-09-03 10:51:22 +0530

    sum code added

--- ranjan.cpp
+++ ranjan.cpp
@@ -9,6 +9,11 @@ void main()
 }
 void mano()
 { 
+int a, b, c;
+cout<<"enter any two number";
+cin>>a>>b;
+c=a+b;
+cout<<"sum="<<c;
 cout<<"manoranjan";
 cout<<"sum="<<c;
    cout<<"have to go there";
