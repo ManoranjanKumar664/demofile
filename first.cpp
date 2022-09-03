30819472d1b66b4d922a916924670be6e9f556f2
Manoranjan Kumar <baskeymanoranjan400@gmail.com>
2022-09-03 21:13:18 +0530

    added

--- first.cpp
+++ first.cpp
@@ -1,10 +1,7 @@
 #include<iosteam.h>
 #include<conio.h>
 void codetowritename();
-<<<<<<< HEAD
-=======
 void oddoreven();
->>>>>>> mano
 void main()
 {
  clrscr();
@@ -15,43 +12,10 @@ c=a+b;
 cout<<"sum="<<c;
  cout<<"hello world";
  codetowritename();
-<<<<<<< HEAD
-=======
 oddoreven();
-<<<<<<< HEAD
->>>>>>> mano
- getch();
- }
-
- void codetowritename()
-{
- if(a<=10)
-{
-cout<<"manoranjan";
-}
-else
-{
- cout<<"not valid";
-}
-<<<<<<< HEAD
-=======
-}
-
-void oddoreven()
-{
- int a;
- cout<<"enter any two number";
-cin>>a;
-if(a%2==0)
-{
-cout<<"even";
-}
-else
-{
- cout<<"odd";
-}
->>>>>>> mano
-=======
 getch();
->>>>>>> mano
+}
+void good()
+{
+cout<<"something added";
 }
