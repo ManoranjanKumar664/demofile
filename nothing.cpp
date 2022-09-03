0011abcbc4fc1a787c53a22d6d04c40a42179cd4
Manoranjan Kumar <baskeymanoranjan400@gmail.com>
2022-09-03 16:19:15 +0530

    new file added

---
+++ nothing.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+     clrscr();
+      cout<<"enter any no.";
+      cin>>a;
+      if(a%2==0)
+      {
+        cout<<"even";
+      }
+      else
+      {
+        cout<<"odd""
+      }
+}
