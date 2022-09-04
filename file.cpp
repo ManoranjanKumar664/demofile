5b0776f69b4738c6a7543f8067536ebc019ee611
Manoranjan Kumar <baskeymanoranjan400@gmail.com>
2022-09-04 14:19:42 +0530

    file.cpp added

---
+++ file.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void name();
+void main()
+{
+clrscr();
+ int i;
+name();
+for(i=1;i<5;i++)
+{
+cout<<"ranjan";
+}
+getch();
+}
+
+void name()
+{
+cout<<"this print you name";
+}
